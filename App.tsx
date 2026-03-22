7f3f387540575508c6f902c92db54c4fb52b89eb
{NikhilAkashMinz} <{nikhilofficial4747@gmail.com}>
2026-03-22 16:31:45 +0530

    started new project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet,Platform,SafeAreaView, Text, Alert,View} from 'react-native';
 
 export default function App() {
+  const onPressButton = () => Alert.alert("Button Pressed");
+  const onLongPressButton = () => Alert.alert("Button Long Pressed");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.View1}>
+          <Text style={styles.text}>Hello World</Text>
+        </View>
+      </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: Platform.OS === 'android' ? 'aqua' : 'red',
     justifyContent: 'center',
+    alignItems: 'center',
+   
   },
+  View1: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image2:{
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+
+  },
+  text:{
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  } 
 });
